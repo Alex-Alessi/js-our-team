5a70693e8b9b5cd640ab79908900be5bdf81e7ba
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-29 14:50:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const membri = [
+  (membro1 = {
+    nome: "MARCO BIANCHI",
+    ruolo: "Designer",
+    email: "marcobianchi@team.com",
+  }),
+  (membro2 = {
+    nome: "LAURA ROSSI",
+    ruolo: "Front-end Developer",
+    email: "laurarossi@team.com",
+  }),
+  (membro3 = {
+    nome: "GIORGIO VERDI",
+    ruolo: "Back-end Developer",
+    email: "giorgoiverdi@team.com",
+  }),
+  (membro4 = {
+    nome: "MARTA IPSUM",
+    ruolo: "SEO Specialist",
+    email: "martaipsum@team.com",
+  }),
+  (membro5 = {
+    nome: "ROBERTO LOREM",
+    ruolo: "SEO Specialist",
+    email: "robertolorem@team.com",
+  }),
+  (membro6 = {
+    nome: "DANIELA AMET",
+    ruolo: "Analyst",
+    email: "danielaamet@team.com",
+  }),
+];
